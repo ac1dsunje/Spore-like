4a9187a361a8303cb02e377f290a4fde95b0a718
ac1dsunje <ar.cherba63programming@gmail.com>
2026-07-17 03:40:29 +0200

    added button for buff slots

--- Assets/_Game/Scripts/Buffs/BuffSlotUI.cs
+++ Assets/_Game/Scripts/Buffs/BuffSlotUI.cs
@@ -7,7 +7,32 @@ namespace _Game.Scripts.Buffs
 public class BuffSlotUI : MonoBehaviour
 {
     [SerializeField] private Image _image;
+    [SerializeField] private Button _button;
     [SerializeField] private TextMeshProUGUI _name;
     [SerializeField] private TextMeshProUGUI _description;
+
+    private int _buffIndex;
+
+    private void OnEnable()
+    {
+        _button.onClick.AddListener(OnButtonClick);
+    }
+
+    public void SetBuff(int buffIndex)
+    {
+        //ToDo: set buff config here
+        _buffIndex = buffIndex;
+        _name.text = $"Buff {buffIndex}";
+    }
+
+    private void OnButtonClick()
+    {
+        Debug.Log($"Buff {_buffIndex} clicked");
+    }
+
+    private void OnDestroy()
+    {
+        _button.onClick.RemoveListener(OnButtonClick);
+    }
 }
 }
--- Assets/_Game/Scripts/UI/BuffChooseScreen.cs
+++ Assets/_Game/Scripts/UI/BuffChooseScreen.cs
@@ -23,6 +23,11 @@ public class BuffChooseScreen: ScreenManager
     private void UpdateLevel(int level)
     {
         ShowScreen();
+        for (var i = 0; i < _slots.Length; i++)
+        {
+            //ToDo: choose buffs to set for slots (actually not here, in other business-logic script)
+            _slots[i].SetBuff(i + 1);
+        }
     }
 
     private void OnDestroy()
