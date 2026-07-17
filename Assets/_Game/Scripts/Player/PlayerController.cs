d62e92c154661ba1a25e336bc990133413cd725e
ac1dsunje <ar.cherba63programming@gmail.com>
2026-07-17 02:15:51 +0200

    added basic player movement

---
+++ Assets/_Game/Scripts/Player/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace _Game.Scripts.Player
+{
+public class PlayerController: MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed;
+
+    private Rigidbody2D _rigidbody;
+
+    private float _horizontalVelocity;
+    private float _verticalVelocity;
+    
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        _horizontalVelocity = Input.GetAxis("Horizontal") * _moveSpeed;
+        _verticalVelocity = Input.GetAxis("Vertical") * _moveSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody.linearVelocity = new Vector2(_horizontalVelocity, _verticalVelocity);
+    }
+}
+}
