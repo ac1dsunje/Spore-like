d4378800c43557edf1d7438dea8ec18bd2c14c85
ac1dsunje <ar.cherba63programming@gmail.com>
2026-07-17 02:57:14 +0200

    added incremental level up logic

--- Assets/_Game/Scripts/Player/PlayerController.cs
+++ Assets/_Game/Scripts/Player/PlayerController.cs
@@ -1,5 +1,4 @@
 ﻿using _Game.Scripts.Food;
-using Unity.VisualScripting;
 using UnityEngine;
 
 namespace _Game.Scripts.Player
@@ -7,7 +6,11 @@ namespace _Game.Scripts.Player
 public class PlayerController: MonoBehaviour, IEater
 {
     [SerializeField] private PlayerConfig _config;
-    [SerializeField] private int _experience; // SerializeField must be deleted after overlay added
+    // SerializeFields must be deleted after overlay added
+    [SerializeField] private int _experience;
+    [SerializeField] private int _level;
+    [SerializeField] private int _levelSet = 2;
+    [SerializeField] private int _levelScaler = 2;
 
     private Rigidbody2D _rigidbody;
 
@@ -17,21 +20,48 @@ public class PlayerController: MonoBehaviour, IEater
     public void Eat(int amount)
     {
         _experience += amount;
+        
+        if (_experience < _levelSet) return;
+        
+        UpdateLevel();
+    }
+
+    private void UpdateLevel()
+    {
+        _experience -= _levelSet;
+        _level++;
+        _levelSet *= _levelScaler;
     }
     
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        Reset();
+    }
+
+    private void Reset()
+    {
         _experience = 0;
+        _level = 0;
     }
 
     private void Update()
+    {
+        GetVelocity();
+    }
+
+    private void GetVelocity()
     {
         _horizontalVelocity = Input.GetAxis("Horizontal") * _config.MoveSpeed;
         _verticalVelocity = Input.GetAxis("Vertical") * _config.MoveSpeed;
     }
 
     private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
     {
         _rigidbody.linearVelocity = new Vector2(_horizontalVelocity, _verticalVelocity);
     }
