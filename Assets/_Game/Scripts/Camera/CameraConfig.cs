5045c759e9cd461ac9c23136b250ba5a4007dd37
ac1dsunje <ar.cherba63programming@gmail.com>
2026-07-19 18:43:12 +0200

    updaed camera config and sand chunk prefab

--- Assets/_Game/Scripts/Camera/CameraConfig.cs
+++ Assets/_Game/Scripts/Camera/CameraConfig.cs
@@ -5,7 +5,7 @@ namespace _Game.Scripts.Camera
 [CreateAssetMenu(fileName = "NewCameraConfig", menuName = "Configs/Game/Camera")]
 public class CameraConfig: ScriptableObject
 {
-    [field: SerializeField, Range(0, 1)] public float FollowSpeed { get; private set; }
+    [field: SerializeField] public float FollowSpeed { get; private set; }
     [field: SerializeField] public Vector3 Offset { get; private set; }
 }
 }
