073d59144d331fcdc9d3c23e5774dfb28326c46f
ac1dsunje <ar.cherba63programming@gmail.com>
2026-07-19 17:32:55 +0200

    added spines

---
+++ Assets/_Game/Scripts/Evolutions/Types/Spines/Spines.cs
@@ -0,0 +1,7 @@
+﻿namespace _Game.Scripts.Evolutions.Types.Spines
+{
+public class Spines: Evolution
+{
+    
+}
+}
---
+++ Assets/_Game/Scripts/Evolutions/Types/Spines/SpinesConfig.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace _Game.Scripts.Evolutions.Types.Spines
+{
+[CreateAssetMenu(fileName = "New Spines Config", menuName = "Configs/Game/Evolutions/Spines")]
+public class SpinesConfig : EvolutionConfig
+{
+    public override Evolution CreateEvolution()
+    {
+        var evo = new Spines();
+        evo.SetConfig(this);
+        return evo;
+    }
+}
+}
