78acdf9f31b0622ef5c72edb1da8346adc199f3c
ac1dsunje <ar.cherba63programming@gmail.com>
2026-08-06 23:21:12 +0200

    added main menu screen

---
+++ Assets/_Game/Scripts/MainMenu/MainMenuScreen.cs
@@ -0,0 +1,11 @@
+﻿using _Game.Scripts.Core.UI;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace _Game.Scripts.MainMenu
+{
+public class MainMenuScreen: UIScreen
+{
+    [SerializeField] private Button _playButton;
+}
+}
