ee55e552d65778e7d94f2b939fecfa86129f0cb3
ac1dsunje <ar.cherba63programming@gmail.com>
2026-08-07 16:11:31 +0200

    added DI for cam in entry point

--- Assets/_Game/Scripts/GamePlay/EntryPoint.cs
+++ Assets/_Game/Scripts/GamePlay/EntryPoint.cs
@@ -3,6 +3,7 @@ using _Game.Scripts.GamePlay.Evolutions.UI.Choosing;
 using _Game.Scripts.GamePlay.Player;
 using _Game.Scripts.GamePlay.UI;
 using _Game.Scripts.GamePlay.World;
+using Unity.Cinemachine;
 using UnityEngine;
 
 namespace _Game.Scripts.GamePlay
@@ -12,7 +13,8 @@ public class EntryPoint : MonoBehaviour
     [SerializeField] private Ticker _ticker;
     [Header("World")]
     [SerializeField] private WorldGenerator _worldGenerator;
-    
+    [Header("Camera")]
+    [SerializeField] private CinemachineCamera _camera;
     [Header("Player")]
     [SerializeField] private PlayerController _player;
 
@@ -29,6 +31,7 @@ public class EntryPoint : MonoBehaviour
         _overlayUIScreen.Construct(_player.Model);
         
         _worldGenerator.Construct(_player.transform);
+        _camera.Target.TrackingTarget = _player.transform;
         
         _evolutionChooseUIScreen.Construct(_player.Model.Evolutions);
         _uiManager.Construct(_evolutionChooseUIScreen, _pauseUIScreen, _player.Model);
