9691a5f47bfcf25cd2f9965e4b37caf142498eb4
ac1dsunje <ar.cherba63programming@gmail.com>
2026-08-11 12:10:31 +0200

    deleted unused directive

--- Assets/_Game/Scripts/GamePlay/Evolutions/Evolution.cs
+++ Assets/_Game/Scripts/GamePlay/Evolutions/Evolution.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using _Game.Scripts.GamePlay.Evolutions.Experience;
 using _Game.Scripts.GamePlay.Player;
 using _Game.Scripts.GamePlay.Rarities;
-using _Game.Scripts.GamePlay.Stats;
 using UnityEngine;
 
 namespace _Game.Scripts.GamePlay.Evolutions
