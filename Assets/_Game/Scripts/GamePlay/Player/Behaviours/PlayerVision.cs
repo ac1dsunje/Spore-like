f3fa39157445f09e57d0650da25f519a7221c179
ac1dsunje <ar.cherba63programming@gmail.com>
2026-08-16 15:39:18 +0200

    smooth vision range update

--- Assets/_Game/Scripts/GamePlay/Player/Behaviours/PlayerVision.cs
+++ Assets/_Game/Scripts/GamePlay/Player/Behaviours/PlayerVision.cs
@@ -11,11 +11,15 @@ public class PlayerVision: EntityNetworkBehaviour
 {
     [SerializeField] private BoxCollider2D _visionCollider;
     [SerializeField] private Light2D _visionLight;
-        
+    [SerializeField] private float _visionChangeSpeed = 5f;
+    
     private VisionModule _module;
     private CinemachineCamera _cineMachine;
     private Camera _camera;
-        
+    
+    private float _targetVision;
+    private float _currentVision;
+
     [Inject]
     private void Construct(VisionModule module, CinemachineCamera cineMachine, Camera cam)
     {
@@ -27,26 +31,47 @@ public class PlayerVision: EntityNetworkBehaviour
     protected override void OnNetworkInitialized()
     {
         _visionCollider.enabled = IsLocal;
+        
         if (IsLocal)
         {
             _module.OnVisionRadiusUpdated += UpdateVision;
             _module.OnLightingUpdated += UpdateLighting;
             
-            UpdateVision(_module.VisionRadius);
+            _currentVision = _module.VisionRadius;
+            _targetVision = _module.VisionRadius;
+            
+            ApplyVision(_currentVision);
         }
+    }
+
+    private void Update()
+    {
+        if (!IsLocal) return;
+
+        if (Mathf.Approximately(_currentVision, _targetVision)) return;
+
+        _currentVision = Mathf.MoveTowards(_currentVision, _targetVision, _visionChangeSpeed * Time.deltaTime);
+
+        ApplyVision(_currentVision);
     }
 
     private void UpdateLighting(float value, bool state)
     {
-        _visionLight.pointLightOuterRadius = state? value : 0f;
+        _visionLight.pointLightOuterRadius = state ? value : 0f;
     }
 
     private void UpdateVision(float value)
     {
-        _visionCollider.size = new Vector2(value * _camera.aspect, value) * 2;
+        _targetVision = value;
+    }
+
+    private void ApplyVision(float value)
+    {
+        _visionCollider.size = new Vector2(value * _camera.aspect, value) * 2f;
+
         _cineMachine.Lens.OrthographicSize = value;
     }
-        
+    
     private void OnTriggerEnter2D(Collider2D other)
     {
         _module.DiscoverGameObject(other.gameObject);
