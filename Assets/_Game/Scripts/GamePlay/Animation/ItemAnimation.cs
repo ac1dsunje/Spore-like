903e79e14fd8e02f8e4bcd752eb3ef9141b82761
ac1dsunje <ar.cherba63programming@gmail.com>
2026-08-17 14:45:45 +0200

    better shadows for big seaweed

--- Assets/_Game/Scripts/GamePlay/Animation/ItemAnimation.cs
+++ Assets/_Game/Scripts/GamePlay/Animation/ItemAnimation.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.Rendering.Universal;
 
 namespace _Game.Scripts.GamePlay.Animation
@@ -6,17 +7,43 @@ namespace _Game.Scripts.GamePlay.Animation
 [RequireComponent(typeof(SpriteRenderer))]
 [RequireComponent(typeof(Animator))]
 [RequireComponent(typeof(ShadowCaster2D))]
+[RequireComponent(typeof(PolygonCollider2D))]
 public class ItemAnimation: MonoBehaviour
 {
     private SpriteRenderer _renderer;
     private Animator _animator;
     private ShadowCaster2D _shadowCaster;
+    private PolygonCollider2D _collider;
 
     private void Awake()
     {
         _renderer = GetComponent<SpriteRenderer>();
         _animator = GetComponent<Animator>();
         _shadowCaster = GetComponent<ShadowCaster2D>();
+        _collider = GetComponent<PolygonCollider2D>();
+    }
+
+    private void LateUpdate()
+    {
+        SetColliderShape(_renderer.sprite);
+    }
+
+    private void SetColliderShape(Sprite sprite)
+    {
+        _collider.enabled = true;
+
+        var shapeCount = sprite.GetPhysicsShapeCount();
+        _collider.pathCount = shapeCount;
+
+        var shape = new List<Vector2>();
+
+        for (var i = 0; i < shapeCount; i++)
+        {
+            shape.Clear();
+
+            sprite.GetPhysicsShape(i, shape);
+            _collider.SetPath(i, shape);
+        }
     }
     
     public void SetVisible(bool visible)
--- Assets/_Game/Scripts/GamePlay/World/Food/FoodController.cs
+++ Assets/_Game/Scripts/GamePlay/World/Food/FoodController.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using _Game.Scripts.GamePlay.Animation;
 using _Game.Scripts.GamePlay.Module;
 using UnityEngine;
@@ -7,7 +6,6 @@ using VContainer;
 
 namespace _Game.Scripts.GamePlay.World.Food
 {
-[RequireComponent(typeof(PolygonCollider2D))]
 public class FoodController: MonoBehaviour
 {
     [Inject] private ItemAnimation _itemAnimation;
@@ -16,53 +14,19 @@ public class FoodController: MonoBehaviour
     [Inject] private EntityStats _stats;
     
     private FoodConfig _config;
-    private PolygonCollider2D _collider;
 
     public event Action<int> OnDeath;
-
-    private void Awake()
-    {
-        _collider = GetComponent<PolygonCollider2D>();
-    }
 
     public void Initialize(FoodConfig config)
     {
         _config = config;
 
-        var sprite = config.AnimationConfig.Sprite;
-
         _itemAnimation.SetConfig(config.AnimationConfig);
-
-        SetColliderShape(sprite);
 
         _health.OnDamageTaken += SpawnParticles;
         _health.OnDeath += Die;
         
         _stats.AddInitialStats(config.StatsConfig.Stats);
-    }
-
-    private void SetColliderShape(Sprite sprite)
-    {
-        if (!sprite)
-        {
-            _collider.enabled = false;
-            return;
-        }
-
-        _collider.enabled = true;
-
-        var shapeCount = sprite.GetPhysicsShapeCount();
-        _collider.pathCount = shapeCount;
-
-        var shape = new List<Vector2>();
-
-        for (var i = 0; i < shapeCount; i++)
-        {
-            shape.Clear();
-
-            sprite.GetPhysicsShape(i, shape);
-            _collider.SetPath(i, shape);
-        }
     }
 
     public void TakeHit(float damage, float penetration)
