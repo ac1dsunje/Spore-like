17cb7aa814ce68e2d0b7b5f69e3cebc035839b14
ac1dsunje <ar.cherba63programming@gmail.com>
2026-08-21 15:37:25 +0200

    moved stattypeData to its file

--- Assets/_Game/Scripts/GamePlay/Stats/Types/StatTypeConfig.cs
+++ Assets/_Game/Scripts/GamePlay/Stats/Types/StatTypeConfig.cs
@@ -54,19 +54,4 @@ public class StatTypeConfig : ScriptableObject
         return data == null ? value : Mathf.Clamp(value, data.MinimalValue, data.MaximalValue);
     }
 }
-
-[Serializable]
-public class StatTypeData
-{
-    [field: SerializeField] public string Name { get; private set; }
-    [field: SerializeField] public StatType Type { get; private set; }
-    [field: SerializeField, Range(-999999, 999999)] public float MinimalValue { get; private set; } = -999999;
-    [field: SerializeField, Range(-999999, 999999)] public float MaximalValue { get; private set; } = 999999;
-
-    public StatTypeData(StatType type)
-    {
-        Type = type;
-        Name = Enum.GetName(typeof(StatType), type);
-    }
-}
 }
