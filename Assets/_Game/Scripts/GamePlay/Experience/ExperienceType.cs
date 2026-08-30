9dfdd6ebffca80039b7948e5cfa8c10596a5e2a0
ac1dsunje <ar.cherba63programming@gmail.com>
2026-08-30 22:34:17 +0200

    moved experience type enum to its file

---
+++ Assets/_Game/Scripts/GamePlay/Experience/ExperienceType.cs
@@ -0,0 +1,21 @@
+﻿namespace _Game.Scripts.GamePlay.Experience
+{
+public enum ExperienceType
+{
+    ObjectDiscover = 0,
+    FoodEating = 1,
+    DamageReflection = 2,
+    DamageResistance = 3,
+    DamageTaking = 4,
+    Healing = 5,
+    DistanceOvercoming = 6,
+    EnduranceRecovering = 7,
+    DisguiseObjectFound = 8,
+    DamageDealing = 9,
+    StartSprinting = 10,
+    DamageBlocking = 11,
+    XRayDiscovering = 12,
+    UnnoticedStaying = 15,
+    UnnoticedStayingInRest = 16,
+}
+}
