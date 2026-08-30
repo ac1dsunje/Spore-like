ba5942f260ff9e897142496d95ee9b78e9268bff
ac1dsunje <ar.cherba63programming@gmail.com>
2026-08-30 21:37:13 +0200

    deleted obstacle for food objects

--- Assets/_Game/Scripts/GamePlay/World/Food/FoodController.cs
+++ Assets/_Game/Scripts/GamePlay/World/Food/FoodController.cs
@@ -9,13 +9,6 @@ public class FoodController: MonoBehaviour
     [Inject] private ItemAnimation _itemAnimation;
     [Inject] private EntityStats _stats;
     [Inject] private FoodHealth _health;
-    
-    private BoxCollider2D _collider;
-
-    private void Awake()
-    {
-        _collider = GetComponent<BoxCollider2D>();
-    }
 
     public void Initialize(FoodConfig config)
     {
@@ -24,7 +17,6 @@ public class FoodController: MonoBehaviour
         _itemAnimation.SetConfig(config.AnimationSettings);
         
         _stats.AddInitialStats(config.StatsConfig.Stats);
-        _collider.isTrigger = !config.IsObstacle;
     }
 }
 }
