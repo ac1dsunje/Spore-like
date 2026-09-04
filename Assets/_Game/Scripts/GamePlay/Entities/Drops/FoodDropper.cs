70ce220848818e4bd18a538b2ca7ff5eb77d14aa
ac1dsunje <ar.cherba63programming@gmail.com>
2026-09-04 11:33:15 +0200

    added food dropper radius

--- Assets/_Game/Scripts/GamePlay/Entities/Drops/FoodDropper.cs
+++ Assets/_Game/Scripts/GamePlay/Entities/Drops/FoodDropper.cs
@@ -6,13 +6,19 @@ namespace _Game.Scripts.GamePlay.Entities.Drops
 public class FoodDropper: MonoBehaviour
 {
     [SerializeField] private FoodDrop _dropPrefab;
+    [SerializeField, Range(0.1f, 5f)] private float _spawnRadius; 
+        
     [Inject] private FoodConfig _foodConfig;
 
     public void Spawn(int amount)
     {
         for (var i = 0; i < amount; i++)
         {
-            var food = Instantiate(_dropPrefab, transform.position, transform.rotation, null);
+            var randomOffset = Random.insideUnitCircle * _spawnRadius;
+            
+            var spawnPosition = transform.position + new Vector3(randomOffset.x, randomOffset.y, 0f);
+            
+            var food = Instantiate(_dropPrefab, spawnPosition, transform.rotation, null);
             food.SetSprite(_foodConfig.Sprite);
         }
     }
