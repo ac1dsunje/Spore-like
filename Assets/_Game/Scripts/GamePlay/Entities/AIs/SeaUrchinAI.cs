9a3ef4dd300de5f959cc2ce8d61dfdd8c8bb71e9
ac1dsunje <ar.cherba63programming@gmail.com>
2026-09-06 18:18:49 +0200

    added evolutions choosing to seaurchin AI

--- Assets/_Game/Scripts/GamePlay/Entities/AIs/SeaUrchinAI.cs
+++ Assets/_Game/Scripts/GamePlay/Entities/AIs/SeaUrchinAI.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using _Game.Scripts.GamePlay.Entities.Attack;
 using _Game.Scripts.GamePlay.Entities.Health;
 using _Game.Scripts.GamePlay.Entities.Hitboxes;
 using _Game.Scripts.GamePlay.Entities.Movement;
+using _Game.Scripts.GamePlay.Evolutions;
 using _Game.Scripts.GamePlay.Interfaces;
 using UnityEngine;
 using VContainer;
@@ -18,6 +20,8 @@ public class SeaUrchinAI : IStartable, ITickable, IDisposable
     [Inject] private IAttackController _attacker;
     [Inject] private IHealthController _healthController;
     [Inject] private BodyHitbox _hitBox;
+    [Inject] private EvolutionsModule _evolutions;
+    [Inject] private EntityModel _model;
 
     private float _directionChangeTimer;
 
@@ -28,6 +32,13 @@ public class SeaUrchinAI : IStartable, ITickable, IDisposable
     {
         _hitBox.OnDamageReceiver += DoDamage;
         _hitBox.OnHit += TakeDamage;
+        _evolutions.OnSlotsFilled += ChooseEvolution;
+    }
+
+    private void ChooseEvolution(List<Evolution> evolutions)
+    {
+        var evolution = evolutions[Random.Range(0, evolutions.Count)];
+        evolution.Apply(_model);
     }
 
     private void TakeDamage(HitInfo hit)
