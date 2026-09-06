c54a51a0361c85e4c087563e0315b1b264b81cb3
ac1dsunje <ar.cherba63programming@gmail.com>
2026-09-06 20:04:40 +0200

    added AttackSpeed safe check for attacking

--- Assets/_Game/Scripts/GamePlay/Entities/Attack/EntityWeaponAttack.cs
+++ Assets/_Game/Scripts/GamePlay/Entities/Attack/EntityWeaponAttack.cs
@@ -16,7 +16,7 @@ public class EntityWeaponAttack : ITickable, IDamageSource, IAttackController
     [Inject] private ProjectileConfig _projectileConfig;
 
     private float _attackCooldownTimer;
-    private bool CanAttack => _attackCooldownTimer <= 0f;
+    private bool CanAttack => _attackCooldownTimer <= 0f && _attack.AttackSpeed >= 0f;
 
     public void Tick()
     {
