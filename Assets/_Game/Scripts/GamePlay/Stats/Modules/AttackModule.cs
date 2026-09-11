c7c4ee7d4304d3cab234826f4da70644e2d18115
ac1dsunje <ar.cherba63programming@gmail.com>
2026-09-11 14:40:49 +0200

    implemented R3 to attack module

--- Assets/_Game/Scripts/GamePlay/Stats/Modules/AttackModule.cs
+++ Assets/_Game/Scripts/GamePlay/Stats/Modules/AttackModule.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using _Game.Scripts.GamePlay.Types;
+﻿using _Game.Scripts.GamePlay.Types;
+using R3;
 
 namespace _Game.Scripts.GamePlay.Modules
 {
@@ -8,16 +8,20 @@ public class AttackModule : StatModule
     public float PhysicalDamage { get; private set; }
     public float IgnoreResistance { get; private set; }
     public float AttackTime { get; private set; }
-    public event Action<float> OnDamageDealt;
-
+    
+    public ReadOnlyReactiveProperty<float> TotalDamaged => _totalDamaged;
+    
+    private readonly ReactiveProperty<float> _totalDamaged = new();
+    
     protected override void Configure()
     {
         BindStat(StatType.PhysicalDamage, UpdatePhysicalDamage);
         BindStat(StatType.IgnoreDamageResistance, UpdateIgnoreResistance);
         BindStat(StatType.AttackTime, UpdateAttackSpeed);
     }
-
-    public void SetDamageDealt(float damage) => OnDamageDealt?.Invoke(damage);
+    
+    public void SetDamageDealt(float damage) => _totalDamaged.Value += damage;
+    
     private void UpdatePhysicalDamage(float value) => PhysicalDamage = value;
     private void UpdateIgnoreResistance(float value) => IgnoreResistance = value;
     private void UpdateAttackSpeed(float value) => AttackTime = value;
