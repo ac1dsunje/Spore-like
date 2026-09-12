036dcbb59fc73d70e761cc7a3fe81bee23e865eb
ac1dsunje <ar.cherba63programming@gmail.com>
2026-09-12 15:49:08 +0200

    implemented uni task to entity attack

--- Assets/_Game/Scripts/GamePlay/Entities/Attack/EntityWeaponAttack.cs
+++ Assets/_Game/Scripts/GamePlay/Entities/Attack/EntityWeaponAttack.cs
@@ -1,9 +1,9 @@
 ﻿using System;
-using System.Collections;
-using _Game.Scripts.Core.Services;
+using System.Threading;
 using _Game.Scripts.GamePlay.Interfaces;
 using _Game.Scripts.GamePlay.Modules;
 using _Game.Scripts.GamePlay.Projectiles;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 namespace _Game.Scripts.GamePlay.Entities.Attack
@@ -15,21 +15,21 @@ public class EntityWeaponAttack : IDamageSource, IAttackController, IDisposable
     private readonly ProjectileSpawner _projectileSpawner;
     private readonly Transform _transform;
     private readonly ProjectileConfig _projectileConfig;
-    private readonly CoroutineRunner _coroutineRunner;
 
-    private const string CooldownKey = "Attack cooldown";
+    private readonly CancellationTokenSource _lifetimeCts = new();
+    
+    private bool _isOnCooldown;
 
-    private bool CanAttack => !_coroutineRunner.IsRunning(this, CooldownKey) && _attack.AttackTime > 0f;
+    private bool CanAttack => !_isOnCooldown && _attack.AttackTime > 0f;
 
     public EntityWeaponAttack(AttackModule attack, IDamageReceiver receiver, ProjectileSpawner projectileSpawner,
-        Transform transform, ProjectileConfig projectileConfig, CoroutineRunner coroutineRunner)
+        Transform transform, ProjectileConfig projectileConfig)
     {
         _attack = attack;
         _receiver = receiver;
         _projectileSpawner = projectileSpawner;
         _transform = transform;
         _projectileConfig = projectileConfig;
-        _coroutineRunner = coroutineRunner;
     }
 
     public void RequestAttack(IDamageReceiver damageReceiver, Vector2 targetPosition)
@@ -39,19 +39,32 @@ public class EntityWeaponAttack : IDamageSource, IAttackController, IDisposable
         var hit = new HitInfo(_attack.PhysicalDamage, _attack.IgnoreResistance, this, _receiver);
         _projectileSpawner.SetAttack(targetPosition, _transform, hit, _projectileConfig);
         
-        _coroutineRunner.Run(this, CooldownKey, CooldownRoutine());
+        _isOnCooldown = true;
+        CooldownAsync().Forget();
     }
 
-    private IEnumerator CooldownRoutine()
+    private async UniTaskVoid CooldownAsync()
     {
-        yield return new WaitForSeconds(_attack.AttackTime);
+        try
+        {
+            await UniTask.Delay(TimeSpan.FromSeconds(_attack.AttackTime), cancellationToken: _lifetimeCts.Token);
+        }
+        catch (OperationCanceledException)
+        {
+            
+        }
+        finally
+        {
+            _isOnCooldown = false;
+        }
     }
     
     public void SetDamageDealt(float damage) => _attack.SetDamageDealt(damage);
 
     public void Dispose()
     {
-        _coroutineRunner.Stop(this);
+        _lifetimeCts.Cancel();
+        _lifetimeCts.Dispose();
     }
 }
 }
